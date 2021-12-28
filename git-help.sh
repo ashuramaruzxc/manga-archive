1f45c7fd31782e0e68af572a1711632be7120d99
Ashley <denisgolovaty@gmail.com>
2021-12-28 22:35:14 +0200

    Добавлено еще чуть-чуть

--- git-help.sh
+++ git-help.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=2009
 
+# TODO: Normale case arguments
+# Which helps to push commits by template
 git add .
-git commit -m "Добавлена новая глава"
+git commit -m ""
 git push -u origin main
