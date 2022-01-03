4dbac902223d2d11afcae9e7a42afbef653e7bc8
Ashley <denisgolovaty@gmail.com>
2022-01-03 21:27:28 +0200

    Added a few pages of manga mostly it's completed but need some time to polish

--- git-help.sh
+++ git-help.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=2009
 
-# TODO: Normale case arguments
+# TODO: Normal case arguments
 # Which helps to push commits by template
+echo -n "Enter commit message: "
+read -r COMMIT
 git add .
-git commit -m ""
+git commit -m "$COMMIT"
 git push -u origin main
