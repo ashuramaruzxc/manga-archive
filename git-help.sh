974532551d9bf1c497a0691e17279f3374859c51
Ashley <denisgolovaty@gmail.com>
2022-01-03 21:29:58 +0200

    also maked git-help.sh a bit more useful

--- git-help.sh
+++ git-help.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck disable=2009
+# shellcheck source=/dev/null
+
+# Fix a relative path
+cd -- "$(dirname "$0")" || exit
 
 # TODO: Normal case arguments
 # Which helps to push commits by template
+
 echo -n "Enter commit message: "
 read -r COMMIT
 git add .
