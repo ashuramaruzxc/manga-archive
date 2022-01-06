dd162d726d40d49d238dcb5bee31d0bd80bb288e
Ashley <denisgolovaty@gmail.com>
2022-01-06 18:30:05 +0200

    fixed page 24

--- git-help.sh
+++ git-help.sh
@@ -8,6 +8,10 @@ cd -- "$(dirname "$0")" || exit
 # TODO: Normal case arguments
 # Which helps to push commits by template
 
+# It's already quiete useful tho but 
+# i think making commit templates will make it more useful
+# So probably will make it next week
+
 echo -n "Enter commit message: "
 read -r COMMIT
 git add .
--- sort.sh
+++ sort.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # shellcheck disable=2009
 
-find ./manga/swibtga/SWIBtGA_ch5\(en\) -iname "*.*" -exec rename . help'{}' \;
+find ./manga/swibtga/SWIBtGA_ch\(en\) -iname "*.*" -exec rename . help'{}' \;
